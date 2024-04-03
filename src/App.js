632484f4d07afdb44d877273762c68471a7d36cc
AGoyal027 <ayushgoyal027@gmail.com>
2024-04-03 22:55:58 +0530

    boilerplate react setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Registration from "./pages/registration/Registration";
+
+function App() {
+  return (
+    <div className="App">
+      <Registration />
+    </div>
+  );
+}
+
+export default App;
